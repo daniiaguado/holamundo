6c56d599e24166a5887c4710340466a50be75410
daagro <daagro@alumnatflorida.es>
2025-02-05 20:00:19 +0100

    Descripción de los cambios

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
  <title>Saludo aleatorio</title>
  <style>
+
  body { font-family: Arial, sans-serif; display: flex; justify-content: center; align-items: center;
  height: 100vh; background-color: #f0f0f0; margin: 0; }
  .container {text-align:center; background-color:#fff; padding:20px; border-radius:8px; box-shadow: 0 0 10px rgba(0,0,0,0.1);}
@@ -25,6 +26,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 </head>
 <body>
  <div class="container">
+    <h1>hola</h1>
  <h1>Generador aleatorio de Hola Mundos</h1>
  <form method="post">
  <button type="submit">Generar</button>
